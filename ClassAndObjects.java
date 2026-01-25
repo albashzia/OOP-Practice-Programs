a4ffc2b8d4df7c412156b5f280d522b17df8c2e2
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-25 19:56:38 +0500

    Assigning values to attributes of objects p1,p2 and p3 of Pen class

--- ClassAndObjects.java
+++ ClassAndObjects.java
@@ -19,6 +19,14 @@ public class ClassAndObjects {
         Pen p1 = new Pen();
         Pen p2 = new Pen();
         Pen p3 = new Pen();
+
+        p1.color = "black";
+        p2.color = "blue";
+        p3.color = "red";
+        
+        p1.type = "gel";
+        p2.type = "ballpoint";
+        p3.type = "pointer";
         
     }
     
