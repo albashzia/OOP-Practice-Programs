1d6262c09f8725034c6d1ab1a6829b6bab50e241
albashzia <ahmedalbashzia@gmail.com>
2026-01-26 21:58:15 +0500

    Adding functions to derived classes

--- Inheritance.java
+++ Inheritance.java
@@ -3,11 +3,17 @@ class Shape{
 }
 
 class Rectangle extends Shape{
+    static void area(int l, int w)
+    {
 
+    }
 }
 
 class Triangle extends Shape{
+    static void area(int base, int height)
+    {
 
+    }
 }
 
 public class Inheritance {
