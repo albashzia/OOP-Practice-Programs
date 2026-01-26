7116bd2d44cc6ff7b03d049ccc029717f7aa6709
albashzia <ahmedalbashzia@gmail.com>
2026-01-26 21:54:08 +0500

    Defining basic structure

--- Inheritance.java
+++ Inheritance.java
@@ -1,3 +1,17 @@
-public class Inheritance {
+class Shape{
 
+}
+
+class Rectangle extends Shape{
+
+}
+
+class Triangle extends Shape{
+
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+
+    }
 }
