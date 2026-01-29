b2617c80f007d61f6d36920ee97e197a022bcd33
albashzia <ahmedalbashzia@gmail.com>
2026-01-29 21:30:17 +0500

    Deleting methods

--- Inheritance.java
+++ Inheritance.java
@@ -3,19 +3,11 @@ class Shape{
 }
 
 class Rectangle extends Shape{
-    static double area(int l, int w)
-    {
-        double areaRect = l * w;
-        return areaRect;
-    }
+
 }
 
 class Triangle extends Shape{
-    static double area(int base, int height)
-    {
-        double areaTri = (0.5)*base*height;
-        return areaTri;
-    }
+
 }
 
 public class Inheritance {
