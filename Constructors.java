51175c31f3008c8d3f26c89acbd5eadade8aaf06
albashzia <ahmedalbashzia@gmail.com>
2026-02-05 21:01:52 +0500

    Creating a non-parameterized constructor and creating an object.

--- Constructors.java
+++ Constructors.java
@@ -2,6 +2,14 @@ class Students{
     int rollNo;
     String name;
     double cgpa;
+
+    Students()
+    {
+        System.out.println("Object Created.");
+    }
 }
 public class Constructors {
+    public static void main(String[] args) {
+        Students s1 = new Students();
+    }
 }
