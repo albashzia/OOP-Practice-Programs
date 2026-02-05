bc1cac6bdd003c1a47174c40f814dafb1bf08ef4
albashzia <ahmedalbashzia@gmail.com>
2026-02-05 21:10:53 +0500

    Creating a parameterized constructor and creating an object by calling the parameterized constructor.

--- Constructors.java
+++ Constructors.java
@@ -7,9 +7,17 @@ class Students{
     {
         System.out.println("Object Created.");
     }
+
+    Students(int rno, String n, double c)
+    {
+        rollNo = rno;
+        name = n;
+        cgpa = c;
+    }
 }
 public class Constructors {
     public static void main(String[] args) {
         Students s1 = new Students();
+        Students s2 = new Students(1001, "Ali", 3.4);
     }
 }
