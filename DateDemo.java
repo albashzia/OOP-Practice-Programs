99af624963bb4f4d66d4da8c1ddace5a931c503e
albashzia <ahmedalbashzia@gmail.com>
2026-02-11 11:56:13 -0800

    Changing the way of initiating class objects.

--- DateDemo.java
+++ DateDemo.java
@@ -10,14 +10,13 @@ class Date{
 
 public class DateDemo {
     public static void main(String[] args) {
-        Date date1, date2;
-        date1 = new Date();
+        Date date1 = new Date();
         date1.month = "December";
         date1.day = 31;
         date1.year = 2012;
         System.out.println("date1:");
         date1.displayDate();
-        date2 = new Date();
+        Date date2 = new Date();
         date2.month = "July";
         date2.day = 4;
         date2.year = 1776;
