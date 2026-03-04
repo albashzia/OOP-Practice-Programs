21ded278be4b3a0ba2bcd9669acefc8301669572
albashzia <ahmedalbashzia@gmail.com>
2026-03-04 11:11:44 +0500

    Adding the Address class.

---
+++ AssociationsDemo1.java
@@ -0,0 +1,55 @@
+class Address {
+    private int streetNo;
+    private int houseNo;
+    private String city;
+    private int code;
+
+    public Address(int streetNo, int houseNo, String city, int code) {
+        this.streetNo = streetNo;
+        this.houseNo = houseNo;
+        this.city = city;
+        this.code = code;
+    }
+
+    public int getStreetNo() {
+        return streetNo;
+    }
+
+    public int getHouseNo() {
+        return houseNo;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setStreetNo(int streetNo) {
+        this.streetNo = streetNo;
+    }
+
+    public void setHouseNo(int houseNo) {
+        this.houseNo = houseNo;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void display() {
+        System.out.println("Street#: " + streetNo +
+                ", House#: " + houseNo +
+                ", City: " + city +
+                ", Code: " + code);
+    }
+}
+
+public class AssociationsDemo1 {
+}
