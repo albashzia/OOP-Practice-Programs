72e5beadebf32f8cb34274857593d62c39609430
albashzia <ahmedalbashzia@gmail.com>
2026-03-04 11:27:01 +0500

    Adding Point1 to PointsDistanceCalculator.java file.

---
+++ PointsDistanceCalculator.java
@@ -0,0 +1,34 @@
+class Point1 {
+
+    private double x;
+    private double y;
+
+    public Point1(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void display() {
+        System.out.println("(" + x + ", " + y + ")");
+    }
+}
+
+
+public class PointsDistanceCalculator {
+}
