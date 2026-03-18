6f0fcfa5ef6f11dc1c048e8cb305dad3a6342393
albashzia <ahmedalbashzia@gmail.com>
2026-03-19 00:22:41 +0500

    Adding files for E_Book System and Ride Sharing System with requirement questions added as comments.

---
+++ E_BookSystem.java
@@ -0,0 +1,14 @@
+/*
+E-Book System
+A publishing company wants to manage different types of books in its catalog.
+All books share basic information such as title, author, and price,
+but some books are digital and include extra details like file size and format (PDF, EPUB, etc.).
+Design a program in Java where a general Book class stores the common attributes,
+while an EBook class extends it with its own specific properties.
+Both types should allow creating objects with default
+values or with complete information, and the system should provide a way to display all
+relevant details for each book.
+*/
+
+public class E_BookSystem {
+}
---
+++ RideSharingSystem.java
@@ -0,0 +1,11 @@
+/*
+Ride Sharing System
+A transportation company develops a ride-sharing system to connect passengers
+with drivers in real time. Each user in the system follows a common structure but has
+different roles and information depending on whether they are a driver or a passenger.
+Drivers store details such as vehicle type, license number, and rating,
+while passengers provide their pickup location and destination. The system tracks
+rides by combining driver and passenger information, calculates fares based on
+*/
+public class RideSharingSystem {
+}
