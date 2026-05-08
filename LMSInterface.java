8c9c963a94dd00416ba36aa8cdfe0cf380fd7432
albashzia <ahmedalbashzia@gmail.com>
2026-05-08 16:26:30 +0500

    Removing the question commented in LMSInterface file

--- LMSInterface.java
+++ LMSInterface.java
@@ -1,17 +1,4 @@
-/***A software company is developing a simple library management system.
- * To manage different types of collections in a uniform way, they design an interface called IterableCollection,
- which provides methods for sequentially accessing elements of a collection.
- The interface defines two methods: hasNext(int index), which checks whether there is another
- element available at the given position, and getNext(int index), which returns the element stored
- at that position.
- To implement this interface, a class called BookCollection is created. This class stores book titles
- in a simple string array. The constructor of the class initializes the array with a set of sample book
- titles.
- The class implements the methods of the interface to allow iteration through the collection. Finally,
- a main method is written to create an object of BookCollection, initialize it with sample data, and
- iterate through all the book titles using the hasNext() and getNext() methods, printing each title on
- the screen.***/
-interface IterableCollection {
+ interface IterableCollection {
 
     boolean hasNext(int index);
 
