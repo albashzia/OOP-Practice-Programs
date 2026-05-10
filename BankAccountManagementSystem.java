76b23fe5ddbb84c26874c99d473be89cec5326e1
albashzia <ahmedalbashzia@gmail.com>
2026-05-10 23:23:58 +0500

    Creating a new file, named BankAccountManagementSystem, adding the requirements question as comment and adding to the project

---
+++ BankAccountManagementSystem.java
@@ -0,0 +1,9 @@
+/***1. Assume that the Account, BusinessAccount, and SavingAccount classes make up the inheritance
+ structure. The account informaƟon includes accountNo, yearOfOpening and balance. Depending
+ on the Account type an interest is to be applied. The interest rate on the BusinessAccount is
+ 15%, while the interest rate on the SavingAccount is 10%. It is must for both these classes to
+ provide funcƟonality for interest applicaƟon.
+ You are required to design class diagram and implement the above scenario. ***/
+
+public class BankAccountManagementSystem {
+}
