2d3fc2868e15ccf8d1d248bfdaabf54a49035bc3
albashzia <ahmedalbashzia@gmail.com>
2026-06-13 19:56:48 +0500

    Accessing the attributes of nested classes and printing the sum of values accessed from both classes

--- InnerClasses.java
+++ InnerClasses.java
@@ -11,5 +11,6 @@ public class InnerClasses {
     public static void main(String[] args) {
         OuterClass myOuterObject = new OuterClass();
         OuterClass.InnerClass myInnerObject = myOuterObject. new InnerClass();
+        System.out.println(myOuterObject.x + myInnerObject.y);
     }
 }
