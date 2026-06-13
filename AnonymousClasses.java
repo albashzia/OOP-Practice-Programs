cee0df990a9bc519e9dc7ed2984662d2bee74a99
albashzia <ahmedalbashzia@gmail.com>
2026-06-13 20:05:15 +0500

    Defining a class named AnimalClass and overriding its method in main method by creating an anonymous class

--- AnonymousClasses.java
+++ AnonymousClasses.java
@@ -1,2 +1,17 @@
+class AnimalClass{
+    void makeSound(){
+
+    }
+}
+
+
 public class AnonymousClasses {
+    public static void main(String[] args) {
+        AnimalClass animal = new AnimalClass(){
+            public void makeSound(){
+                System.out.println("Woof Woof");
+            }
+        };
+        animal.makeSound();
+    }
 }
