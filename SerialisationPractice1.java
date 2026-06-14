c92275ed64837fa8c4eea785fa3a22ad161f7f34
albashzia <ahmedalbashzia@gmail.com>
2026-06-14 22:40:32 +0500

    Defining the default and parameterized constructors of Student2 class

--- SerialisationPractice1.java
+++ SerialisationPractice1.java
@@ -1,6 +1,16 @@
 class Student2{
     int id;
     String name;
+
+    Student2(){
+        id = 0;
+        name = null;
+    }
+
+    Student2(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
 }
 
 
