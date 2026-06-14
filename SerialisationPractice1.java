39331586a38d521c1e914317e298743b5bce6039
albashzia <ahmedalbashzia@gmail.com>
2026-06-14 23:27:21 +0500

    Writing objects to the serialized file using ObjectOutputStream

--- SerialisationPractice1.java
+++ SerialisationPractice1.java
@@ -1,5 +1,4 @@
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 
 class Student2 implements Serializable{
     int id;
@@ -18,8 +17,17 @@ class Student2 implements Serializable{
 
 
 public class SerialisationPractice1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Student2 s1 = new Student2();
         Student2 s2 = new Student2(101,"Max");
+
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("students.ser"));
+            oos.writeObject(s1);
+            oos.writeObject(s2);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
